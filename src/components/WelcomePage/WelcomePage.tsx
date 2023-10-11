aa6298552952cab4af4145aa23aa4ea0f9897af4
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-10-11 08:25:26 +0200

    test for another branch

--- src/components/WelcomePage/WelcomePage.tsx
+++ src/components/WelcomePage/WelcomePage.tsx
@@ -6,6 +6,7 @@ function WelcomePage() {
   return (
     <>
       <h1>This is the title for Welcome Page</h1>
+      <p>This has been a test to another branch</p>
       <button
         onClick={() => {
           setCount((count: number) => count + 1);
